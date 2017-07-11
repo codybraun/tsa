5b3abb3cac2ea0dea2919c9b3e9367111d92af00
codybraun <codybraun@gmail.com>
2017-07-10 22:47:15 -0600

    Fitting single zone

---
+++ Binary+Classifier.py
@@ -0,0 +1,302 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import numpy as np
+import matplotlib as mpl
+import os
+import matplotlib.pyplot as plt
+import matplotlib.animation
+import png
+import tensorflow as tf
+from numpy import genfromtxt
+import pandas as pd
+
+
+# In[2]:
+
+TEST_SIZE = 5
+
+
+# In[3]:
+
+def read_header(infile):
+    """Read image header (first 512 bytes)
+    """
+    h = dict()
+    fid = open(infile, 'r+b')
+    h['filename'] = b''.join(np.fromfile(fid, dtype = 'S1', count = 20))
+    h['parent_filename'] = b''.join(np.fromfile(fid, dtype = 'S1', count = 20))
+    h['comments1'] = b''.join(np.fromfile(fid, dtype = 'S1', count = 80))
+    h['comments2'] = b''.join(np.fromfile(fid, dtype = 'S1', count = 80))
+    h['energy_type'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['config_type'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['file_type'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['trans_type'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['scan_type'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['data_type'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['date_modified'] = b''.join(np.fromfile(fid, dtype = 'S1', count = 16))
+    h['frequency'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['mat_velocity'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['num_pts'] = np.fromfile(fid, dtype = np.int32, count = 1)
+    h['num_polarization_channels'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['spare00'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['adc_min_voltage'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['adc_max_voltage'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['band_width'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['spare01'] = np.fromfile(fid, dtype = np.int16, count = 5)
+    h['polarization_type'] = np.fromfile(fid, dtype = np.int16, count = 4)
+    h['record_header_size'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['word_type'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['word_precision'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['min_data_value'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['max_data_value'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['avg_data_value'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['data_scale_factor'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['data_units'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['surf_removal'] = np.fromfile(fid, dtype = np.uint16, count = 1)
+    h['edge_weighting'] = np.fromfile(fid, dtype = np.uint16, count = 1)
+    h['x_units'] = np.fromfile(fid, dtype = np.uint16, count = 1)
+    h['y_units'] = np.fromfile(fid, dtype = np.uint16, count = 1)
+    h['z_units'] = np.fromfile(fid, dtype = np.uint16, count = 1)
+    h['t_units'] = np.fromfile(fid, dtype = np.uint16, count = 1)
+    h['spare02'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['x_return_speed'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['y_return_speed'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['z_return_speed'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['scan_orientation'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['scan_direction'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['data_storage_order'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['scanner_type'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['x_inc'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['y_inc'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['z_inc'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['t_inc'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['num_x_pts'] = np.fromfile(fid, dtype = np.int32, count = 1)
+    h['num_y_pts'] = np.fromfile(fid, dtype = np.int32, count = 1)
+    h['num_z_pts'] = np.fromfile(fid, dtype = np.int32, count = 1)
+    h['num_t_pts'] = np.fromfile(fid, dtype = np.int32, count = 1)
+    h['x_speed'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['y_speed'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['z_speed'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['x_acc'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['y_acc'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['z_acc'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['x_motor_res'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['y_motor_res'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['z_motor_res'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['x_encoder_res'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['y_encoder_res'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['z_encoder_res'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['date_processed'] = b''.join(np.fromfile(fid, dtype = 'S1', count = 8))
+    h['time_processed'] = b''.join(np.fromfile(fid, dtype = 'S1', count = 8))
+    h['depth_recon'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['x_max_travel'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['y_max_travel'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['elevation_offset_angle'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['roll_offset_angle'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['z_max_travel'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['azimuth_offset_angle'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['adc_type'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['spare06'] = np.fromfile(fid, dtype = np.int16, count = 1)
+    h['scanner_radius'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['x_offset'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['y_offset'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['z_offset'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['t_delay'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['range_gate_start'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['range_gate_end'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['ahis_software_version'] = np.fromfile(fid, dtype = np.float32, count = 1)
+    h['spare_end'] = np.fromfile(fid, dtype = np.float32, count = 10)
+    return h
+
+
+# In[4]:
+
+def read_data(infile):
+    """Read any of the 4 types of image files, returns a numpy array of the image contents
+    """
+    extension = os.path.splitext(infile)[1]
+    h = read_header(infile)
+    nx = int(h['num_x_pts'])
+    ny = int(h['num_y_pts'])
+    nt = int(h['num_t_pts'])
+    fid = open(infile, 'rb')
+    fid.seek(512) #skip header
+    if extension == '.aps' or extension == '.a3daps':
+        if(h['word_type']==7): #float32
+            data = np.fromfile(fid, dtype = np.float32, count = nx * ny * nt)
+        elif(h['word_type']==4): #uint16
+            data = np.fromfile(fid, dtype = np.uint16, count = nx * ny * nt)
+        data = data * h['data_scale_factor'] #scaling factor
+        data = data.reshape(nx, ny, nt, order='F').copy() #make N-d image
+    elif extension == '.a3d':
+        if(h['word_type']==7): #float32
+            data = np.fromfile(fid, dtype = np.float32, count = nx * ny * nt)
+        elif(h['word_type']==4): #uint16
+            data = np.fromfile(fid, dtype = np.uint16, count = nx * ny * nt)
+        data = data * h['data_scale_factor'] #scaling factor
+        data = data.reshape(nx, nt, ny, order='F').copy() #make N-d image
+    elif extension == '.ahi':
+        data = np.fromfile(fid, dtype = np.float32, count = 2* nx * ny * nt)
+        data = data.reshape(2, ny, nx, nt, order='F').copy()
+        real = data[0,:,:,:].copy()
+        imag = data[1,:,:,:].copy()
+    fid.close()
+    if extension != '.ahi':
+        return data
+    else:
+        return real, imag
+
+
+# In[5]:
+
+def bulk_read_data(image_paths):
+    data = []
+    for path in image_paths:
+        path = "./images/" + path + ".aps"
+        data.append(read_data(path))
+    return np.stack(data)
+
+
+# In[23]:
+
+def build_model(data, labels, mode):
+    data = tf.reshape(data, [-1, 512, 660, 16])
+    print(data.shape)
+    conv = tf.layers.conv2d(
+        inputs=data,
+        filters=32,
+        kernel_size=[50, 50],
+        padding="same",
+        activation=tf.nn.relu)
+    conv = tf.reshape(conv, [10, 512, 660, 32])
+    print ("CONV " + str(conv))
+    pool1 = tf.layers.max_pooling2d(inputs=conv, pool_size=[2, 2], strides=2)
+    print("POOL " + str(pool1))
+    flat_pool = tf.reshape(pool1, [-1, 2703360])
+    print ("FLAT POOL " + str(flat_pool))
+    logits = tf.layers.dense(inputs=flat_pool, units=2)
+    logits =tf.identity(logits, name="logits")
+    logits = tf.reshape(logits, [10,2])
+    print("LOGITS " + str(logits))
+    #labels = tf.one_hot(labels, depth=2)
+    flat_labels = tf.reshape(labels, [10, 2])
+    test_labels=tf.identity(flat_labels, name="labels")
+    print("LABELS " + str(test_labels))
+    loss = tf.losses.softmax_cross_entropy(onehot_labels=test_labels, logits=logits)
+    #loss = tf.losses.mean_squared_error(test_labels, logits)
+    predictions = {
+        "classes": tf.argmax(
+          input=logits, axis=1, name="classes"),
+      "probabilities": tf.nn.softmax(
+          logits, name="softmax_tensor")}
+    print(predictions)
+    train_op = tf.contrib.layers.optimize_loss(
+        loss=loss,
+        global_step=tf.contrib.framework.get_global_step(),
+        learning_rate=0.001,
+        optimizer="SGD")
+    return tf.contrib.learn.ModelFnOps(mode=mode, predictions=predictions, loss=loss, train_op=train_op)
+
+
+# In[24]:
+
+image_df = pd.read_csv('stage1_labels.csv')
+image_df['zone'] = image_df['Id'].str.split("_", expand=True)[1].str.strip()
+image_df['id'] = image_df['Id'].str.split("_", expand=True)[0].str.strip()
+
+
+# In[25]:
+
+def input_images(ids):
+    data = bulk_read_data(ids)
+    print("data shape " + str(data.shape))
+    return data
+
+
+# In[26]:
+
+def input_labels(zone, ids):
+    test_labels = image_df[image_df['id'].isin(ids)]
+    test_labels = test_labels[test_labels["zone"]==zone]
+    test_labels["class0"] = 0
+    test_labels["class1"] = 0
+    test_labels.loc[test_labels['Probability'] == 0, 'class0'] = 1
+    test_labels.loc[test_labels['Probability'] == 1, 'class1'] = 1
+#     test_labels = np.reshape(np.array(test_labels["Probability"].tolist()), [-1,1])
+    test_labels = np.reshape(np.array(test_labels[["class0","class1"]]), [-1,2])
+    print("Label shape " + str(test_labels.shape))
+    return test_labels
+
+
+# In[27]:
+
+tf.reset_default_graph()
+sess = tf.InteractiveSession()
+
+
+# In[28]:
+
+sess.run(tf.initialize_all_variables())
+
+
+# In[30]:
+
+tsa_classifier = tf.contrib.learn.Estimator(model_fn=build_model, model_dir="/tmp/tsa_model7")
+
+
+# In[31]:
+
+tensors_to_log = {"probabilities": "softmax_tensor", "classes":"classes", "actual":"labels", "logits":"logits"}
+logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=5)
+
+
+# In[ ]:
+
+ids = image_df["id"].unique()[:100]
+tsa_classifier.fit(x=input_images(ids), y=input_labels("Zone1",ids), steps=10000, batch_size=10, monitors=[logging_hook])
+
+
+# In[16]:
+
+filters = tsa_classifier.get_variable_value("conv2d/kernel")
+
+
+# In[17]:
+
+filters.shape
+
+
+# In[22]:
+
+x = filters[:,:,0,0]
+plt.imshow(x)
+plt.show()
+
+
+# In[21]:
+
+dir(tf.python_io)
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
